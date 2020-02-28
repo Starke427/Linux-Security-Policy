51b980bae948b0b55e78b00fd320ae9f4fd869e2
Starke427 <jstarke@superuser.life>
2020-02-28 15:15:14 -0700

    Create auditd_setup_ubuntu16.sh

---
+++ auditd_setup_ubuntu16.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+# Automate Debian8/Ubuntu16 advanced auditd configuration.
+# Author: Jeff Starke
+
+sudo cp /etc/audit/rules.d/audit.rules /etc/audit/rules.d/audit.rules.bak
+sudo cat > /etc/audit/rules.d/audit.rules <<\EOF
+# This file contains the auditctl rules that are loaded when the audit daemon is started via initscripts.
+# The rules are parameters that would be passed to auditctl.
+# This rule set is specifically for Debian8/Ubuntu16.
+# Credit to NNT for compiling these audit rules. newnetttechnologies.com
+
+# Config Rules
+-D		#removes all previous rules
+-b  3074	#define buffer size
+-f 4		#panic on failure
+-r 120		#create at most 120 audit messages per second
+
+# Date/Time
+-a always,exit -F arch=b64 -S adjtimex -S settimeofday -k time-change
+-a always,exit -F arch=b32 -S adjtimex -S settimeofday -S stime -k time-change
+-a always,exit -F arch=b64 -S clock_settime -k time-change
+-a always,exit -F arch=b32 -S clock_settime -k time-change
+-w /etc/localtime -p wa -k time-change
+
+# User/Group
+-w /etc/group -p wa -k identity
+-w /etc/passwd -p wa -k identity
+-w /etc/gshadow -p wa -k identity
+-w /etc/shadow -p wa -k identity
+-w /etc/security/opasswd -p wa -k identity
+
+# Network Environment
+-a always,exit -F arch=b64 -S sethostname -S setdomainname -k system-locale
+-a always,exit -F arch=b32 -S sethostname -S setdomainname -k system-locale
+-w /etc/issue -p wa -k system-locale
+-w /etc/issue.net -p wa -k system-locale
+-w /etc/hosts -p wa -k system-locale
+-w /etc/sysconfig/network -p wa -k system-locale
+
+# Mandatory Access Controls
+-w /etc/selinux/ -p wa -k MAC-policy
+
+# Login/Logout
+-w /var/log/faillog -p wa -k logins
+-w /var/log/lastlog -p wa -k logins
+-w /var/log/tallylog -p wa -k logins
+
+# Session Initiation
+-w /var/run/utmp -p wa -k session
+-w /var/log/wtmp -p wa -k session
+-w /var/log/btmp -p wa -k session
+
+# Discretionary Access Controls
+-a always,exit -F arch=b64 -S chmod -S fchmod -S fchmodat -F auid>=1000 -F auid!=4294967295 -k perm_mod
+-a always,exit -F arch=b32 -S chmod -S fchmod -S fchmodat -F auid>=1000 -F auid!=4294967295 -k perm_mod
+-a always,exit -F arch=b64 -S chown -S fchown -S fchownat -S lchown -F auid>=1000 -F auid!=4294967295 -k perm_mod
+-a always,exit -F arch=b32 -S chown -S fchown -S fchownat -S lchown -F auid>=1000 -F auid!=4294967295 -k perm_mod
+-a always,exit -F arch=b64 -S setxattr -S lsetxattr -S fsetxattr -S removexattr -S lremovexattr -S fremovexattr -F auid>=1000 -F auid!=4294967295 -k perm_mod
+-a always,exit -F arch=b32 -S setxattr -S lsetxattr -S fsetxattr -S removexattr -S lremovexattr -S fremovexattr -F auid>=1000 -F auid!=4294967295 -k perm_mod
+
+# Unsuccessful File Access Attempts
+-a always,exit -F arch=b64 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EACCES -F auid>=1000 -F auid!=4294967295 -k access
+-a always,exit -F arch=b32 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EACCES -F auid>=1000 -F auid!=4294967295 -k access
+-a always,exit -F arch=b64 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EPERM -F auid>=1000 -F auid!=4294967295 -k access
+-a always,exit -F arch=b32 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EPERM -F auid>=1000 -F auid!=4294967295 -k access
+
+# Use of Privileged Commands
+-a always,exit -F path=$file -F perm=x -F auid>=1000 -F auid!=4294967295 -k privileged
+
+# Successful File System Mounts
+-a always,exit -F arch=b64 -S mount -F auid>=1000 -F auid!=4294967295 -k mounts
+-a always,exit -F arch=b32 -S mount -F auid>=1000 -F auid!=4294967295 -k mounts
+
+# File Deletion Events by Users
+-a always,exit -F arch=b64 -S unlink -S unlinkat -S rename -S renameat -F auid>=1000 -F auid!=4294967295 -k delete
+-a always,exit -F arch=b32 -S unlink -S unlinkat -S rename -S renameat -F auid>=1000 -F auid!=4294967295 -k delete
+
+# System Admin Scope (Sudoers)
+-w /etc/sudoers -p wa -k scope
+
+# System Admin Actions (Sudolog)
+-w /var/log/sudo.log -p wa -k actions
+
+# Kernel Module Loading/Unloading
+-w /sbin/insmod -p x -k modules
+-w /sbin/rmmod -p x -k modules
+-w /sbin/modprobe -p x -k modules
+-a always,exit -F arch=b64 -S init_module -S delete_module -k modules
+-a always,exit -F arch=b32 -S init_module -S delete_module -k modules
+
+# Immutability Settings
+-e 1	#enable auditing
+-e 2	#make the configuration immutable -- reboot is required to change audit rules
+EOF
+
+sudo service auditd reload
